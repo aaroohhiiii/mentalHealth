f48ad95673253a17c34d0873a39b726728d53a27
Karthik M Sarma <147068646+i-am-batman-28@users.noreply.github.com>
2025-11-03 23:51:50 +0530

    Deleting

--- archive/scripts/start_backend.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /Users/karthiksarma/mentalHealth/mentalHealth/backend
-export PYTHONNOUSERSITE=1
-/Users/karthiksarma/mentalHealth/mentalHealth/backend/venv/bin/python -m uvicorn app:app --reload --host 0.0.0.0 --port 8000
